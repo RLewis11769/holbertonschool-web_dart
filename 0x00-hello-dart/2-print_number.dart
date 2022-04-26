9f8feddef1ac111c29a5a5c73e2e79e36dc41193
Rachel Lewis <RLewis11769@gmail.com>
2022-04-25 22:15:19 -0500

    Add data to 2-print_number

---
+++ 0x00-hello-dart/2-print_number.dart
@@ -0,0 +1,8 @@
+// Complete source code to print integer stored in variable
+// Can't print number and string together with +
+// So use string interpolation (could also use n.toString())
+
+void main() {
+  var n = 98;
+  print('$n Battery Street\n');
+}
