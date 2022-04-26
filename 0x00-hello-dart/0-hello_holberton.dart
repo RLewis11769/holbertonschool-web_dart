7660150831b83ac59382bfb69ea2d63d307677cb
Rachel Lewis <RLewis11769@gmail.com>
2022-04-25 22:11:56 -0500

    Adding 1-quotes and editing 0-hello

--- 0x00-hello-dart/0-hello_holberton.dart
+++ 0x00-hello-dart/0-hello_holberton.dart
@@ -1,6 +1,6 @@
 // Print Hello Holberton! followed by newline
-// Example shows 2 newlines, but Dart includes newline at end of string so not adding second
+// Example shows 2 newlines, since Dart includes newline at end of string
 
 void main() {
-  print('Hello Holberton!');
+  print('Hello Holberton!\n');
 }
