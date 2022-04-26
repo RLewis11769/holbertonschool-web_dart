978db547ec7302dded8f59cb8cdfdd265fe7c9bb
Rachel Lewis <RLewis11769@gmail.com>
2022-04-25 22:20:23 -0500

    Add data to 3-print_double

---
+++ 0x00-hello-dart/3-print_double.dart
@@ -0,0 +1,7 @@
+// Complete source code to print double with precision of 2 digits
+// Fix number of digits to 2
+
+void main() {
+  var n = 3.14159265359;
+  print('Double: ' + n.toStringAsFixed(2) + '\n');
+}
