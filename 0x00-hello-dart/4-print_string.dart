e0e0267578043e94ef0f607ec993298e6c808429
Rachel Lewis <RLewis11769@gmail.com>
2022-04-25 22:25:50 -0500

    Add data to 4-print_string

---
+++ 0x00-hello-dart/4-print_string.dart
@@ -0,0 +1,9 @@
+// Complete source code to print string 3 times, then first 9 characters
+// * is String multiply operator
+// substring is like slice - start inclusive to end exclusive (optional)
+
+void main() {
+  String str = "Holberton School";
+  print(str * 3);
+  print(str.substring(0, 9) + '\n');
+}
