58d0f4e9fc8d2bd3f703c721723fba65ba87568d
Rachel Lewis <RLewis11769@gmail.com>
2022-04-27 20:33:14 -0500

    Potentially fix code-length error - not sure what issue is

--- 0x00-hello-dart/2-print_number.dart
+++ 0x00-hello-dart/2-print_number.dart
@@ -1,7 +1,4 @@
-// Print integer stored in variable
-// Can't print number and string together with + so use string interpolation (could also use n.toString())
-
-void main() {
+void main() { // Print integer stored in variable
   var n = 98;
-  print('$n Battery street\n');
+  print("$n Battery street\n"); // Can't print number and string together with + so use string interpolation (could also use n.toString())
 }
