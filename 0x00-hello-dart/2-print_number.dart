c022b053978ec2e507b4136bc5cdba7a3cadebd2
Rachel Lewis <RLewis11769@gmail.com>
2022-04-27 20:38:30 -0500

    This looks like casting to string but is using $ as asked so idk

--- 0x00-hello-dart/2-print_number.dart
+++ 0x00-hello-dart/2-print_number.dart
@@ -2,6 +2,6 @@ import "dart:io";
 
 void main() { // Print integer stored in variable
   var n = 98;
-  stdout.write(n); // Print without newline
+  stdout.write("$n"); // Print without newline
   print(" Battery street\n");
 }
