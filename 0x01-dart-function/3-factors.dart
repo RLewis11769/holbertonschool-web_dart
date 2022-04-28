80627da5303bf1dccbd63a6e0a7de5d1d40fb0ed
Rachel Lewis <RLewis11769@gmail.com>
2022-04-27 20:41:15 -0500

    Why was the function name changed halfway through the project...

--- 0x01-dart-function/3-factors.dart
+++ 0x01-dart-function/3-factors.dart
@@ -1,7 +1,7 @@
 // Recursive function to compute factors of positive integer
 // Factors for 5 are 5 * 4 * 3 * 2 * 1 = 120
 
-int factor(int f) {
+int fact(int f) {
   // Edge case - 0 and negative numbers don't have factors
   if (f <= 0) return 0;
 
@@ -11,6 +11,6 @@ int factor(int f) {
   } else {
     // When f > 1, save 5 and recurse with 4 for example
     // 5 * factor(4) computes to 4 * factor(3) etc until left with 5 * 4 * 3 * 2 * 1
-    return f * factor(f - 1);
+    return f * fact(f - 1);
   }
 }
