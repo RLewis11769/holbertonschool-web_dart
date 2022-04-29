4c9d5baefb0277f18f18ecf59774c83b75cbbb98
Rachel Lewis <RLewis11769@gmail.com>
2022-04-28 19:41:19 -0500

    Wrap in try/catch block

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -2,22 +2,26 @@ import "4-main.dart";
 import "dart:convert";
 
 Future<double> calculateTotal() {
-  // Call fetchUserData
-  return fetchUserData().then((user) {
-    // Convert string response to json
-    var id = jsonDecode(user)["id"];
-    // Asynchronously call fetchUserOrders with id of user from fetchUserData
-    return fetchUserOrders(id).then((products) async {
-      var prices = 0.0;
-      // Convert string response to json and loop through product list
-      for (var product in jsonDecode(products)) {
-        // As each product is fetched, call fetchProductPrice with product
-        await fetchProductPrice(product).then((price) {
-          // Convert string response to json and add price to total
-          prices += jsonDecode(price);
-        });
-      }
-      return prices;
+  try {
+    // Call fetchUserData
+    return fetchUserData().then((user) {
+      // Convert string response to json
+      var id = json.decode(user)["id"];
+      // Asynchronously call fetchUserOrders with id of user from fetchUserData
+      return fetchUserOrders(id).then((products) async {
+        var prices = 0.0;
+        // Convert string response to json and loop through product list
+        for (var product in json.decode(products)) {
+          // As each product is fetched, call fetchProductPrice with product
+          await fetchProductPrice(product).then((price) {
+            // Convert string response to json and add price to total
+            prices += json.decode(price);
+          });
+        }
+        return prices;
+      });
     });
-  });
+  } catch (e) {
+    return Future.error(-1);
+  }
 }
